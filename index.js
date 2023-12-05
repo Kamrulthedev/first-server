88f7740dd71c93b485ac3f691f0366bacbee146c
Kamrul Hassan <kamrulthedev@gmail.com>
2023-12-05 15:29:09 +0600

    phones-commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
-const phones = require('./Phone-shop.json')
+const phones = require('./Phone-shop.json');
 const app = express()
 const port = 5000;
 app.get('/',(req, res)=>{
-    res.send('my secount server to the golbal sserver this is my kamrul hassa')
+    res.send('my secount server to the golbal sserver this is my kamrul hassan')
 })
+
+
+app.get('/Phones', (req, res)=>{
+    res.send(phones)
+})
+
+app.get('/Phones/:id', (req, res)=>{
+    const id = parseInt(req.params.id)
+
+    console.log('Phoenes.id',id)
+    const phone = phones.find(phone => phone.id)|| {};
+    res.send(phone)
+})
+
 
 app.listen(port, ()=>{
     console.log(`my secoutn servsre,  ${port}`)
